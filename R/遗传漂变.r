14828937e527d65cb2d1479fba10bf1620b7a0d6
Deer-T <75743219+Deer-T@users.noreply.github.com>
2020-12-10 00:14:19 +0800

    Create 遗传漂变.r

---
+++ R/遗传漂变.r
@@ -0,0 +1,22 @@
+drift<-function(N,p0,G,times){
+  M<-data.frame()
+  for(m in 1:times){
+    A<-2*N*p0
+    for(i in 1:G){
+      p<-A/(2*N)  #杂合子比例
+      A<-rbinom(1,2*N,p)
+      M[m,i]<-p
+    }}
+    return(M)
+}
+value<-drift(300,0.5,5000,50)
+value2<-drift(3000,0.5,5000,50)
+A<-value[,5000]
+B<-value2[,5000]
+
+df<-data.frame(A,B)  
+
+library("ggplot2")
+
+ggplot(df)+geom_histogram(aes(A),fill="red",alpha=0.5)+
+  geom_histogram(aes(B),fill="green",alpha=0.5)+xlab("hetero")
